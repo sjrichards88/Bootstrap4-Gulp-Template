e5a1c6d6f33840b2e4ff9f4a2969e77dea80abab
Simon Richards <s.richards@dewynters.com>
2017-04-19 23:08:59 +0100

    Tidy

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,27 @@ var gulp = require('gulp');
 	cleancss = require('gulp-clean-css'),
 	rename = require('gulp-rename'),
 	jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
 	plumber = require('gulp-plumber'),
 	browserSync = require('browser-sync').create();
 
 // Project directories
 var config = {
-    jQueryDir: './node_modules/jquery',
-    tetherDir: './node_modules/tether',
-    publicDir: './build',
-    projectScssDir: './src/scss',
-    projectJsDir: './src/js'
+	jQueryDir: './node_modules/jquery',
+	tetherDir: './node_modules/tether',
+	publicDir: './build',
+	projectScssDir: './src/scss',
+	projectJsDir: './src/js'
 };
 
 // Start browserSync server
 gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-          baseDir: config.publicDir
-        }
-    });
+	browserSync.init({
+		server: {
+			baseDir: config.publicDir
+		}
+	});
 });
 
 // Compile our scss
@@ -34,15 +34,15 @@ gulp.task('scss', function() {
 	var plugins = [
 		autoprefixer({
 			browsers: [
-	            "Android 2.3",
-	            "Android >= 4",
-	            "Chrome >= 20",
-	            "Firefox >= 24",
-	            "Explorer >= 8",
-	            "iOS >= 6",
-	            "Opera >= 12",
-	            "Safari >= 6"
-	        ],
+				"Android 2.3",
+				"Android >= 4",
+				"Chrome >= 20",
+				"Firefox >= 24",
+				"Explorer >= 8",
+				"iOS >= 6",
+				"Opera >= 12",
+				"Safari >= 6"
+			],
 			cascade: false
 		}),
 		require('postcss-flexbugs-fixes')
@@ -51,13 +51,13 @@ gulp.task('scss', function() {
 	.pipe(plumber())
 	.pipe(sourcemaps.init())
 	.pipe(sass({
-        precision: 8,
-        style: 'compressed'
-    }).on('error', sass.logError))
+		precision: 8,
+		style: 'compressed'
+	}).on('error', sass.logError))
 	.pipe(postcss(plugins))
 	.pipe(rename({ suffix: '.min' }))
-    .pipe(cleancss())
-    .pipe(sourcemaps.write('.'))
+	.pipe(cleancss())
+	.pipe(sourcemaps.write('.'))
 	.pipe(gulp.dest(config.publicDir + '/assets/css'))
 	.pipe(browserSync.reload({ // Reloading with Browser Sync
          stream: true
@@ -66,16 +66,16 @@ gulp.task('scss', function() {
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src(config.projectJsDir + '/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+	return gulp.src(config.projectJsDir + '/*.js')
+	.pipe(jshint())
+	.pipe(jshint.reporter('default'));
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src([
-        config.jQueryDir + '/dist/jquery.min.js',
-        config.tetherDir + '/dist/js/tether.min.js',
+	return gulp.src([
+		config.jQueryDir + '/dist/jquery.min.js',
+		config.tetherDir + '/dist/js/tether.min.js',
 		//config.projectJsDir + '/bootstrap/alert.js',
 		//config.projectJsDir + '/bootstrap/button.js',
 		//config.projectJsDir + '/bootstrap/carousel.js',
@@ -87,24 +87,24 @@ gulp.task('scripts', function() {
 		//config.projectJsDir + '/bootstrap/tab.js',
 		//config.projectJsDir + '/bootstrap/tooltip.js',
 		//config.projectJsDir + '/bootstrap/util.js',
-        config.projectJsDir + '/vendor/*.js',
-        config.projectJsDir + '/*.js',
-    ])
+		config.projectJsDir + '/vendor/*.js',
+		config.projectJsDir + '/*.js',
+	])
 	.pipe(plumber())
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest(config.publicDir + '/assets/js'))
-    .pipe(rename('main.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest(config.publicDir + '/assets/js'))
-    .pipe(browserSync.reload({ // Reloading with Browser Sync
-        stream: true
+	.pipe(concat('main.js'))
+	.pipe(gulp.dest(config.publicDir + '/assets/js'))
+	.pipe(rename('main.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest(config.publicDir + '/assets/js'))
+	.pipe(browserSync.reload({ // Reloading with Browser Sync
+		stream: true
      }));
 });
 
 // Watch Files For Changes
 gulp.task('watch', ['browserSync'], function() {
-    gulp.watch(config.projectJsDir + '/**/*.js', ['lint', 'scripts']);
-    gulp.watch(config.projectScssDir + '/**/*.scss', ['scss']);
+	gulp.watch(config.projectJsDir + '/**/*.js', ['lint', 'scripts']);
+	gulp.watch(config.projectScssDir + '/**/*.scss', ['scss']);
     gulp.watch(config.publicDir + '/**/*.html').on('change', browserSync.reload);
 });
 
